17dfb41ab37368212fbd81b0becb387ea983ff75
Yuliiasergiivna <krempolska@gmail.com>
2025-11-13 12:04:39 +0100

    ScoreController

--- src/Controller/ScoresController.php
+++ src/Controller/ScoresController.php
@@ -1,19 +1,24 @@
 <?php
 namespace App\Controller;
 
+use App\Service\GameEngine;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class ScoresController extends AbstractController
 {
     // Page "Tableau des scores"
-    #[Route('/scores', name: 'scores_index')]
-    public function index(): Response
-    {
+    #[Route('/scores', name: 'app_scores')]
+    public function scores(GameEngine $engine): Response
+    {    
+        $scores = $engine ->calculerScoresMoyens();
         // Pour l’instant on affiche une page vide/placeholder.
         // On pourra y injecter les vrais scores plus tard.
-        return $this->render('scores/index.html.twig');
+        return $this->render('scores/index.html.twig', [
+            'scores' => $scores,
+        ]);
     }
 }
 
